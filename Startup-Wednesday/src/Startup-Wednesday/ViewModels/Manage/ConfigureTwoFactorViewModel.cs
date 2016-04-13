9b0d10a984b73f2914db32071d970002ad24c9c1
andreosebastiani <andreosebastiani@gmail.com>
2016-04-13 09:01:13 -0400

    Adding .net 5 template with identity authentication

---
+++ Startup-Wednesday/src/Startup-Wednesday/ViewModels/Manage/ConfigureTwoFactorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace Startup_Wednesday.ViewModels.Manage
+{
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+
+        public ICollection<SelectListItem> Providers { get; set; }
+    }
+}
